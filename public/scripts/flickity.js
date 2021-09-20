92f2551936602e2dd32e11efc1e7b5efb6ad8ace
Kalkidan Alemu <kali.taye@gmail.com>
2021-09-20 12:26:19 -0400

    Add favourites page,  flickity.js

---
+++ public/scripts/flickity.js
@@ -0,0 +1,9 @@
+var elem = document.querySelector('.grid-slider-wrap');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'center',
+  contain: true,
+  autoPlay: true,
+  freeScroll: false
+
+});
--- server.js
+++ server.js
@@ -50,6 +50,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/favourites", (req, res) => {
+  res.render("favourites");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
