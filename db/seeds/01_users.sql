2e0357eee8757fbb8702903529b66ec75b3ee422
kadia <kdabraeux@gmail.com>
2021-09-21 15:11:35 +0000

    pdated scss

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,7 +1,4 @@
 <<<<<<< HEAD
 -- Users table seeds here (Example)
-=======
--- -- Users table seeds here (Example)
->>>>>>> 18fbeda23edbc881c54d43938becf4f298441358
--- INSERT INTO users (name) VALUES ('Alice');
--- INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name) VALUES ('Alice');
+INSERT INTO users (name) VALUES ('Kira');
