af877459899216460f3c5ebea2de3b74a48166e3
kadia <kdabraeux@gmail.com>
2021-09-24 13:48:56 +0000

    cleaned up db haalpers file and servers file

--- db/database-helper.js
+++ db/database-helper.js
@@ -1,106 +1,99 @@
 const helpers = function (db) {
-  //function to get items for Category page
+  //loads items for each category page
   const getItems = function () {
     return db
-      .query(`SELECT * FROM items;`)
-      .then((result) => {
+      .query (`SELECT * FROM items;`)
+      .then (result => {
         return result.rows;
       })
-      .catch((err) => {
-        console.log("error!", err.message);
+      .catch (err => {
+        console.log ('error!', err.message);
       });
   };
 
-  //function to get users
   const getUser = function (users_id) {
     return db
-      .query(`SELECT * FROM users WHERE users.id = $1;`, [users_id])
-      .then((result) => {
+      .query (`SELECT * FROM users WHERE users.id = $1;`, [users_id])
+      .then (result => {
         return result.rows;
       })
-      .catch((err) => {
-        console.log("error!", err.message);
+      .catch (err => {
+        console.log ('error!', err.message);
       });
   };
 
-  // Functions to verify if user is a seller
+  //Varifies whether a user is a seller or not
   const getSeller = function (seller_id) {
     return db
-      .query(`SELECT * FROM items WHERE seller_id = $1;`, [seller_id])
-      .then((result) => {
+      .query (`SELECT * FROM items WHERE seller_id = $1;`, [seller_id])
+      .then (result => {
         return result.rows;
       })
-      .catch((err) => {
-        console.log("error!", err.message);
+      .catch (err => {
+        console.log ('error!', err.message);
       });
   };
 
+  //gets thus user that has both a user and seller id
   const getUserSeller = function (user_id) {
-    return (
-      db
-        //  .query(`SELECT * FROM items WHERE seller_id is NOT NULL AND user_id = $1;`, [user_id])
-        .query(
-          `
+    return db
+      .query (
+        `
         SELECT *
         FROM users
         JOIN items ON items.seller_id = users.id WHERE users.id = $1 AND seller_id is NOT NULL;`,
-          [user_id]
-        )
-        .then((result) => {
-          return result.rows;
-        })
-        .catch((err) => {
-          console.log("error!", err.message);
-        })
-    );
+        [user_id]
+      )
+      .then (result => {
+        return result.rows;
+      })
+      .catch (err => {
+        console.log ('error!', err.message);
+      });
   };
 
   const getUserByEmail = function (email) {
     return db
-      .query(`SELECT * FROM users WHERE email = $1;`, [email])
-      .then((result) => {
+      .query (`SELECT * FROM users WHERE email = $1;`, [email])
+      .then (result => {
         return result.rows[0];
       })
-      .catch((err) => {
-        console.log("error!", err.message);
+      .catch (err => {
+        console.log ('error!', err.message);
       })
-      .catch((err) => {
-        console.log("error!", err.message);
+      .catch (err => {
+        console.log ('error!', err.message);
       });
   };
 
-  //call get items for given min and max price will go to values, else renders default
-  // so ask users what min and max price they WebAuthentication. also check webs
+  //allows users to be able to filter price using minimum-maximum price input
   const getItemsByPrice = function (minPrice = 0, maxPrice = 10000000) {
-    // condition
-    //btwn function
     let queryString = `SELECT * FROM items WHERE price >= $1 AND price <= $2;`;
     return db
-      .query(queryString, [minPrice, maxPrice])
-      .then((result) => {
-        // console.log("minPrice: ", minPrice);
-        console.log(".....Result.rows.length: ", result.rows.length);
+      .query (queryString, [minPrice, maxPrice])
+      .then (result => {
+        console.log ('.....Result.rows.length: ', result.rows.length);
         return result.rows;
       })
-      .catch((err) => {
-        console.log("error!", err.message);
+      .catch (err => {
+        console.log ('error!', err.message);
       });
   };
 
   const getCategory = function (category) {
     return db
-      .query(`SELECT * FROM items WHERE category = $1;`, [category])
-      .then((result) => {
+      .query (`SELECT * FROM items WHERE category = $1;`, [category])
+      .then (result => {
         return result.rows;
       })
-      .catch((err) => {
-        console.log("error!", err.message);
+      .catch (err => {
+        console.log ('error!', err.message);
       });
   };
 
   const getFavourites = function (user_id) {
     return db
-      .query(
+      .query (
         `
         SELECT *
         FROM items
@@ -109,27 +102,27 @@ const helpers = function (db) {
         `,
         [user_id]
       )
-      .then((result) => {
-        console.log("user_id: ", user_id);
+      .then (result => {
+        console.log ('user_id: ', user_id);
         return result.rows;
       })
-      .catch((err) => {
-        console.log("error!", err.message);
+      .catch (err => {
+        console.log ('error!', err.message);
       });
   };
 
-  //make db filter helper function to select item by id with a WHERE in there.
   const getItem = function (items_id) {
     return db
-      .query(`SELECT * FROM items WHERE items.id = $1;`, [items_id])
-      .then((result) => {
+      .query (`SELECT * FROM items WHERE items.id = $1;`, [items_id])
+      .then (result => {
         return result.rows;
       })
-      .catch((err) => {
-        console.log("error!", err.message);
+      .catch (err => {
+        console.log ('error!', err.message);
       });
   };
 
+  //Adds a item to a users favourite item database
   const addFavourites = function (user_id, item_id) {
     const queryString = `
     INSERT INTO favourite_items (items_id, user_id)
@@ -137,22 +130,17 @@ const helpers = function (db) {
     RETURNING *;
     `;
 
-    const values = [
-      item_id,
-      user_id
-      ]
-
+    const values = [item_id, user_id];
     return db
-      .query(queryString, values)
-      .then((result) => {
-        console.log("Result.rows: ", result.rows[0]);
+      .query (queryString, values)
+      .then (result => {
+        console.log ('Result.rows: ', result.rows[0]);
         return result.rows[0];
       })
-      .catch((err) => console.log(err.message));
+      .catch (err => console.log (err.message));
   };
 
   const itemSold = function (item_id) {
-    // SELECT * FROM items WHERE buyer_id IS NOT NULL;
     const queryString = `
     UPDATE items
     SET sold = true
@@ -160,34 +148,28 @@ const helpers = function (db) {
     `;
     const values = [item_id];
     return db
-      .query(queryString, values)
-      .then((result) => {
-        console.log("Result.rows: ", result.rows[0]);
+      .query (queryString, values)
+      .then (result => {
+        console.log ('Result.rows: ', result.rows[0]);
         return result.rows[0];
       })
-      .catch((err) => console.log(err.message));
+      .catch (err => console.log (err.message));
   };
 
-  //returns undefined items.id and {}
+  //Allows only the admin to delete an item
   const removeItem = function (item_id) {
     const queryString = `
     DELETE FROM items WHERE items.id = $1;`;
-    // const values =
-
     return db
-      .query(queryString, [item_id])
-      .then((result) => {
-        console.log("------> GOOD TO GO!!!!!");
-        // return result.rows[0];
+      .query (queryString, [item_id])
+      .then (result => {
+        console.log ('------> GOOD TO GO!!!!!');
       })
-      .catch((err) => console.log(err.message));
+      .catch (err => console.log (err.message));
   };
 
-  // ADD new item
-  const postItem = function(newItem) {
-// ('Ruched Sweetheart Neck Velvet Bodycon Dress', 26.00, 'xoxo green velvet body dress'
-//  'https://img.ltwebstatic.com/images3_pi/2020/11/20/16058551232dc741f0c163e3f8015a95bfe50edafa.webp', 'https://img.ltwebstatic.com/images3_pi/2020/11/20/16058551232dc741f0c163e3f8015a95bfe50edafa_thumbnail_600x.webp',3 , 1, false, 1);
-
+  //Adds/lists a new item to the items database
+  const postItem = function (newItem) {
     const queryString = `
     INSERT INTO items (
       title, price, description, cover_photo_url, thumbnail_photo_url, category, user_id, sold, seller_id)
@@ -204,22 +186,38 @@ const helpers = function (db) {
       newItem.category,
       newItem.user_id,
       newItem.sold,
-      newItem.seller_id
+      newItem.seller_id,
     ];
 
-
     return db
-      .query(queryString, values)
-      .then((result) => {
-        console.log("-------> new item on its way ------> ", result);
+      .query (queryString, values)
+      .then (result => {
+        console.log ('-------> new item on its way ------> ', result);
         return result.rows;
       })
-      .catch((err) => console.log(err.message, "hmmmm....didn't function like it was supposed to."))
+      .catch (err =>
+        console.log (
+          err.message,
+          "hmmmm....didn't function like it was supposed to."
+        )
+      );
+  };
 
-    };
-
-
-return {getItems, getFavourites, getItem, getCategory, getItemsByPrice, getUser, getUserByEmail, addFavourites, removeItem, getSeller, getUserSeller, itemSold, postItem };
+  return {
+    getItems,
+    getFavourites,
+    getItem,
+    getCategory,
+    getItemsByPrice,
+    getUser,
+    getUserByEmail,
+    addFavourites,
+    removeItem,
+    getSeller,
+    getUserSeller,
+    itemSold,
+    postItem,
+  };
 };
 
 module.exports = helpers;
--- server.js
+++ server.js
@@ -1,337 +1,263 @@
-//terminal end that catches client.js all requests
 // load .env data into process.env
-require("dotenv").config();
+require ('dotenv').config ();
 
-// Web server config
+// ____________________Web server config_______________________________
 const PORT = process.env.PORT || 8080;
-const ENV = process.env.ENV || "development";
-const express = require("express");
-const bodyParser = require("body-parser");
-const sass = require("node-sass-middleware");
-const app = express();
-const morgan = require("morgan");
+const ENV = process.env.ENV || 'development';
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const sass = require ('node-sass-middleware');
+const app = express ();
+const morgan = require ('morgan');
 const bcryptjs = require ('bcryptjs');
-const cookieSession = require("cookie-session");
-app.use(cookieSession({
-  name: "session",
-  keys: ["eSgVkYp3s6v9y$B&E)H@McQfTjWmZq4t", "z$C&F)J@NcRfUjWnZr4u7x!A%D*G-KaP" ]
-}));
+const cookieSession = require ('cookie-session');
+app.use (
+  cookieSession ({
+    name: 'session',
+    keys: [
+      'eSgVkYp3s6v9y$B&E)H@McQfTjWmZq4t',
+      'z$C&F)J@NcRfUjWnZr4u7x!A%D*G-KaP',
+    ],
+  })
+);
 
-// PG database client/connection setup
-const { Pool } = require("pg");
-const dbParams = require("./lib/db.js");
-const db = new Pool(dbParams);
-db.connect();
+// ________________PG database client/connection setup_________________
+const {Pool} = require ('pg');
+const dbParams = require ('./lib/db.js');
+const db = new Pool (dbParams);
+db.connect ();
 //requires helper function and directly calls db
-const databaseHelpers = require("./db/database-helper")(db);
+const databaseHelpers = require ('./db/database-helper') (db);
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan("dev"));
+app.use (morgan ('dev'));
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  "/styles",
-  sass({
-    src: __dirname + "/styles",
-    dest: __dirname + "/public/styles",
+app.set ('view engine', 'ejs');
+app.use (bodyParser.urlencoded ({extended: true}));
+app.use (
+  '/styles',
+  sass ({
+    src: __dirname + '/styles',
+    dest: __dirname + '/public/styles',
     debug: true,
-    outputStyle: "expanded",
+    outputStyle: 'expanded',
   })
 );
-app.use(express.static("public"));
+app.use (express.static ('public'));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const usersRoutes = require ('./routes/users');
+const widgetsRoutes = require ('./routes/widgets');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use ('/api/users', usersRoutes (db));
+app.use ('/api/widgets', widgetsRoutes (db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-app.get("/", (req, res) => {
 
-  // req.session.user_id = req.params.users.id;
 
-  // console.log('++++user undefined check', user)
-  // console.log('++++req.params undefined check',req.params)
+// ________ALL * GET * REQUESTS START HERE_____________________________
 
-  // if (!req.sessions.user_id) {
-  //   res.render("index");
-  // }
-  res.render("index");
-  // res.redirect (`/`);
+app.get ('/', (req, res) => {
+  res.render ('index');
 });
 
-
-app.get("/_header", (req, res) => {
-
-  const user = req.session
-  // const templateVars = { user, urls: urlDatabase };
-
-  res.render("/home", {user});
+app.get ('/_header', (req, res) => {
+  const user = req.session;
+  res.render ('/home', {user});
 });
 
-app.post("/login", (req, res) => {
-  const tmpPassword = bcryptjs.hashSync('123')
-  //  console.log(">>>>>>>>", tmpPassword)
-  const email = req.body.email;
-  const password = req.body.password;
-  const templateVars = {};
+app.get ('/home', (req, res) => {
+  res.render ('index');
+});
 
+app.get ('/home/logged_in', (req, res) => {
+  res.render ('logged_in');
+});
 
-  if (!email || !password) {
-    templateVars.error = 'email and password cannot be empty'
-    return res.render("index", templateVars);
-  }
-
-  databaseHelpers.getUserByEmail(email).then((user) => {
-    // console.log(">>>>>>>>>>result: ", user);
-    if (!user) {
-      console.log("user not found.........")
-      templateVars.error = 'No account plz register'
-      return res.render("index", templateVars);
-    }
-    //compares both inputed passwords
-    const checkPassword = bcryptjs.compareSync(password, user.password);
-    if (!checkPassword){
-      // console.log("wrong password .........")
-      templateVars.error = 'Wrong password'
-      return res.render("index", templateVars)
-    }
-    req.session.user_id = user.id;
-    // const user = users[req.session["userID"]];
-    console.log("++++++++", req.session.user_id)
-    templateVars.user = user;
-    console.log("+++++++++++++++++user+++++++++++++>>>", user);
-    res.render("index", user);
+app.get ('/home/:category', (req, res) => {
+  req.session.user_id;
+  databaseHelpers.getCategory (req.params.category).then (result => {
+    console.log ('result: ', result);
+    res.render ('category', {items: result, user_id: req.session.user_id});
   });
 });
 
-
-
-
-// POST /logout
-app.post("/logout", (req,res) => {
-  //
-  // req.session.user_id = user.id;
-  req.session = null;
-  // console.log("*********req.session[user_id]********:" , req.session["user_id"]);
-  return res.redirect("/home");
-
-});
-
-
-
-app.get("/home", (req, res) => {
-  res.render("index");
-});
-
-app.get("/home/logged_in", (req, res) => {
-  res.render("logged_in");
-});
-
-app.get("/home/:category", (req, res) => {
-  req.session.user_id
-  databaseHelpers.getCategory(req.params.category)
-    .then((result) => {
-      console.log("result: ", result);
-      res.render("category", {items: result, user_id:  req.session.user_id });
-
-  })
-  //if statements with 3 item page routes. if button 1 clicked res.render first item page etc.
-});
-
-app.get("/item_description", (req, res) => {
-  //set the default price
-  const minP =req.params.minPrice || 0
-  const maxP =req.params.maxPrice || 10000000
-
-  //wanna see numbers inputted
-  console.log("<<<<<<<<<< minmax req.params price",req.params);
-  databaseHelpers.getItemsByPrice(minP, maxP)
-  .then((result) => {
-    // console.log("result: ", result);
-    res.render("item_description", {items: result});
-
-  })
-})
-
-app.post("/item_description", (req, res) => {
-  //set the default price
-  console.log("<<<<<<<<<< minmax req.body price",req.body);
-  const minP =req.body.minPrice || 0
-  const maxP =req.body.maxPrice || 10000000
-
-  //wanna see numbers inputted
-
-  databaseHelpers.getItemsByPrice(minP, maxP)
-  .then((result) => {
-    // console.log("result: ", result);
-    res.render("item_description", {items: result});
-
-  })
-});
-
-app.get("/item_description/:id", (req, res) => {
-  //*IMP*req.params.id is assoc with whatevr name is after : in route name
-  databaseHelpers.getItem(req.params.id).then((result) => {
-    console.log("result: ", result);
-    res.render("item_description", { items: result });
+app.get ('/item_description', (req, res) => {
+  //sets the default price
+  const minP = req.params.minPrice || 0;
+  const maxP = req.params.maxPrice || 10000000;
+  console.log ('<<<<<<<<<< minmax req.params price', req.params);
+  databaseHelpers.getItemsByPrice (minP, maxP).then (result => {
+    res.render ('item_description', {items: result});
   });
 });
 
-// GET admin page for admin only
-//QUESTION--cant find seller id
-app.get("/admin", (req, res) => {
+app.get ('/item_description/:id', (req, res) => {
   //*IMP*req.params.id is assoc with whatevr name is after : in route name
-  console.log("req.session``````: ", req.session);
-  // req.session.user_id = user.id;
-  const tmpPassword = bcryptjs.hashSync('123');
+  databaseHelpers.getItem (req.params.id).then (result => {
+    console.log ('result: ', result);
+    res.render ('item_description', {items: result});
+  });
+});
+
+app.get ('/admin', (req, res) => {
+  //*IMP*req.params.id is assoc with whatevr name is after : in route name
+  console.log ('req.session``````: ', req.session);
+  const tmpPassword = bcryptjs.hashSync ('123');
   const user_id = req.session.user_id;
   const item_id = req.body.item_id;
   const templateVars = {};
-  console.log("---------USER ID: ", user_id)
+  console.log ('---------USER ID: ', user_id);
 
-  if(user_id) {
-  console.log(("...User is logged in....."), user_id)
+  if (user_id) {
+    console.log ('...User is logged in.....', user_id);
   } else {
-    return res.status(401).send("You do not have access to this page");
+    return res.status (401).send ('You do not have access to this page');
   }
-  databaseHelpers.getUserSeller(user_id).then((result) => {
-    console.log("result: ", result);
-    res.render("admin", { items: result, user: user_id });
+  databaseHelpers.getUserSeller (user_id).then (result => {
+    console.log ('result: ', result);
+    res.render ('admin', {items: result, user: user_id});
   });
   // res.render("admin", { items: result });
 });
-// /route/:id, req.params.id=assoc with :id in route, res.render to id specific page
-
-// app.get("/favourites", (req, res) => {
-//   databaseHelpers.getItems().then((result) => {
-//     console.log("result: ", result);
-//     res.render("favourites", { items: result });
-//   });
-// });
 
 //show a specific users fav page, ex favourites/1
-app.get("/favourites/:id", (req, res) => {
-  databaseHelpers.getFavourites(req.params.id).then((result) => {
-    console.log("<<<<<<<<<<req.params.id:", req.params.id);
-    console.log("result: ", result);
-    res.render("favourites", { items: result });
+app.get ('/favourites/:id', (req, res) => {
+  databaseHelpers.getFavourites (req.params.id).then (result => {
+    console.log ('<<<<<<<<<<req.params.id:', req.params.id);
+    console.log ('result: ', result);
+    res.render ('favourites', {items: result});
   });
 });
-//make route for /favourites/summary page for each user
 
+app.get ('/new_item', (req, res) => {
+  res.render ('new_item');
+});
+
+
+// __________ALL * POST * REQUESTS START HERE__________________________
+
+app.post ('/item_description', (req, res) => {
+  console.log ('<<<<<<<<<< minmax req.body price', req.body);
+  const minP = req.body.minPrice || 0;
+  const maxP = req.body.maxPrice || 10000000;
+  databaseHelpers.getItemsByPrice (minP, maxP).then (result => {
+    res.render ('item_description', {items: result});
+  });
+});
 
 //favourites button functionality to add fav item to db w/o re-direct due to UX
-app.post("/favourites", (req, res) => {
-  const tmpPassword = bcryptjs.hashSync('123')
-  // req.session.user_id = user.id;
-  // req.session.item_id = items.id;
-  const user_id = req.session.user_id
-  const item_id = req.body.item_id
+app.post ('/favourites', (req, res) => {
+  const tmpPassword = bcryptjs.hashSync ('123');
+  const user_id = req.session.user_id;
+  const item_id = req.body.item_id;
   const templateVars = {};
-  // console.log('value of the heart++++++req.session',req.session)
-  console.log('.......user_id, item_id',user_id, item_id)
-  databaseHelpers.addFavourites(user_id, item_id).then((result) => {
-    console.log("result: ", result);
-   return res.json({ items: result });
+  console.log ('.......user_id, item_id', user_id, item_id);
+  databaseHelpers.addFavourites (user_id, item_id).then (result => {
+    console.log ('result: ', result);
+    return res.json ({items: result});
   });
 });
 
-
-app.post("/item/sold", (req, res) => {
-  const tmpPassword = bcryptjs.hashSync('123')
-  const user_id = req.session.user_id
-  const item_id = req.body.item_id
+app.post ('/item/sold', (req, res) => {
+  const tmpPassword = bcryptjs.hashSync ('123');
+  const user_id = req.session.user_id;
+  const item_id = req.body.item_id;
   const templateVars = {};
   if (item_id) {
-    console.log("...ITEM exists to be SOLD ..");
+    console.log ('...ITEM exists to be SOLD ..');
   }
-  databaseHelpers.itemSold(item_id).then((result) => {
-    console.log("result: ", result);
-   return res.json({ items: result });
+  databaseHelpers.itemSold (item_id).then (result => {
+    console.log ('result: ', result);
+    return res.json ({items: result});
   });
 });
 
 //POST DELETE items by seller only
-app.post("/item/delete", (req, res) => {
-
-  const tmpPassword = bcryptjs.hashSync('123');
-  // const seller_id = req.params.seller_id;
-  const user_id = req.session.user_id
+app.post ('/item/delete', (req, res) => {
+  const tmpPassword = bcryptjs.hashSync ('123');
+  const user_id = req.session.user_id;
   const item_id = req.body.item_id;
 
-  console.log("delete item_id......... :", item_id);
+  console.log ('delete item_id......... :', item_id);
   const templateVars = {};
 
   if (user_id) {
-    console.log("...Seller is here.....");
+    console.log ('...Seller is here.....');
   }
-  databaseHelpers.removeItem(item_id).then((result) => {
-    console.log("====== item DELETED =======")
-  return res.status(200).json({status: ">>>>> item deleted!!!! <<<<<<"});
+  databaseHelpers.removeItem (item_id).then (result => {
+    console.log ('====== item DELETED =======');
+    return res.status (200).json ({status: '>>>>> item deleted!!!! <<<<<<'});
   });
-  // delete urlDatabase[shortURL];
-  // console.log("result: ", result);
-  // res.redirect("/urls");
 });
 
-//____for mark sold function____
-// const buyer_id = req.params.shortURL;
-// if (buyer_id) {
-  //   return res.status(401).send("Authorization required to delete this short URL.");
-  //   //   console.log("userID", userID);
-  //   // } else {
-    // //   const urlsUser = urlsForUser(userID, urlDatabase);
-    // //   console.log("urlsUser: ", urlsUser);
-    //   if (urlsUser) {
-//     console.log(`Delete shortURL request sent:`, req.params.shortURL);
-//     return res.redirect("/urls");
-//   // } else {
-//   //   return res.status(401).send("You do not have access to this URL");
-//   // }
-// }
-
-//GET new items
-app.get("/new_item", (req, res) => {
-
-     res.render("new_item");
-
-});
-
-//GET new items
-app.post("/new_item", (req, res) => {
-
-  const tmpPassword = bcryptjs.hashSync('123');
+app.post ('/new_item', (req, res) => {
+  const tmpPassword = bcryptjs.hashSync ('123');
   // const seller_id = req.params.seller_id;
   const user_id = req.session.user_id;
   const item = req.body;
   const newItem = user_id && item;
 
-  console.log()
+  console.log ();
 
-
-  console.log("++++++++", req.body)
-   databaseHelpers.postItem(newItem).then((result) => {
-    console.log("========item_id:", user_id);
-    console.log("result: ", result);
-    res.render("new_item", { items: result });
+  console.log ('++++++++', req.body);
+  databaseHelpers.postItem (newItem).then (result => {
+    console.log ('========item_id:', user_id);
+    console.log ('result: ', result);
+    res.render ('new_item', {items: result});
   });
-  res.redirect("/admin");
+  res.redirect ('/admin');
+});
+
+app.post ('/login', (req, res) => {
+  const tmpPassword = bcryptjs.hashSync ('123');
+  const email = req.body.email;
+  const password = req.body.password;
+  const templateVars = {};
+
+  if (!email || !password) {
+    templateVars.error = 'email and password cannot be empty';
+    return res.render ('index', templateVars);
+  }
+
+  databaseHelpers.getUserByEmail (email).then (user => {
+    if (!user) {
+      console.log ('user not found.........');
+      templateVars.error = 'No account plz register';
+      return res.render ('index', templateVars);
+    }
+    //compares both inputed passwords
+    const checkPassword = bcryptjs.compareSync (password, user.password);
+    if (!checkPassword) {
+      templateVars.error = 'Wrong password';
+      return res.render ('index', templateVars);
+    }
+    req.session.user_id = user.id;
+    console.log ('++++++++', req.session.user_id);
+    templateVars.user = user;
+    console.log ('+++++++++++++++++user+++++++++++++>>>', user);
+    res.render ('index', user);
+  });
+});
+
+app.post ('/logout', (req, res) => {
+  req.session = null;
+  return res.redirect ('/home');
 });
 
 
+
+
+
 // ____________PORT______________
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+app.listen (PORT, () => {
+  console.log (`Example app listening on port ${PORT}`);
 });
